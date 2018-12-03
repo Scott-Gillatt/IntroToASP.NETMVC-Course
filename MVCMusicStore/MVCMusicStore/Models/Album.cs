bb3045a4c227616943ca389099c5fa1db3bb68ad
Scott-Gillatt <16550268+Scott-Gillatt@users.noreply.github.com>
2018-12-03 10:23:32 -0700

    Created Album Model

---
+++ MVCMusicStore/MVCMusicStore/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+	public class Album
+	{
+		#region Public Properties
+
+		public int AlbumID { get; set; }
+
+		#endregion Public Properties
+	}
+}
